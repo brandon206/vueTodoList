fb17a5190827ea2ba7659b43abe90ec85a68ad00
Brandon Park <brandonpark206@gmail.com>
2019-04-20 18:01:51 -0700

    initial rendering of components

---
+++ src/components/Todos.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- v-for goes in a loop -->
+        <!-- todoItem refers to each object inside the todos list that was passed in -->
+        <!-- todos referes to the array of todos that was passed in from props -->
+        <!-- <div v-bind:key="todoItem.id" v-for="todoItem in todos">
+            <h3>{{todoItem.title}}</h3>
+        </div> -->
+
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: "Todos",
+    components: {
+        TodoItem
+    },
+    props: ["todos"]
+}
+</script>
+
+
+<style scoped>
+
+</style>
